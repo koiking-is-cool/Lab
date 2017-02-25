c1414a1c07790fb1f810f5b25d10b7a40aab51c4
NEW\Miki <nattakan.eimo@bumail.net>
2017-02-25 13:11:38 +0700

    Edit add snow

---
+++ Lab/Assets/code/SceneBallController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneBallController : MonoBehaviour {
+	public GameObject obtaclePrefab;
+	// Use this for initialization
+	void Start () {
+		DoTestObtacle ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DoChangeScene(){
+		SceneManager.LoadScene(1);
+	}
+
+	public void DoTestObtacle(){
+		StartCoroutine (GameProcess ());
+	}
+
+	IEnumerator GameProcess(){
+		for (int i=0;i<10;i++){
+			Instantiate (obtaclePrefab);
+			yield return new WaitForSeconds (1);
+		}
+	}
+}
